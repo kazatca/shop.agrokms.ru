c7b99758391ef5504909ea45ee6c445828c8ac0d
kazatca <kazatca@gmail.com>
2017-04-04 11:02:13 +1000

    added rollup

---
+++ server/rollup.config.js
@@ -0,0 +1,28 @@
+import {readdirSync} from 'fs';
+import babel from 'rollup-plugin-babel';
+import json from 'rollup-plugin-json';
+
+const externals = readdirSync('../node_modules');
+
+export default {
+  entry: 'src/index.js',
+  sourceMap: true,
+  format: 'cjs',
+  plugins: [
+    babel({
+      exclude: 'node_modules/**',
+      babelrc: false,
+      presets: [
+        ["es2015", {modules: false}],
+        "react",
+        "stage-2"
+      ],
+      plugins: [
+        'external-helpers'
+      ]
+    }),
+    json()
+  ],
+  external: [...externals, 'fs', 'react-dom/server'],
+  dest: 'dist/bundle.js'
+};
