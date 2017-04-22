be4a393e5c6f532836d31f1347f63d761bf43003
kazatca <kazatca@gmail.com>
2017-04-22 13:17:04 +1000

    use development env

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -27,7 +27,12 @@ module.exports = {
     extensions: ['.js', '.jsx']
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"development"'
+      }
+    })
   ],
   devServer: {
     contentBase: './dist',
