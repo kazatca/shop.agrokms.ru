529787c4063b4ced0eec925c9a16902d14da2867
kazatca <kazatca@gmail.com>
2017-04-25 16:09:20 +1000

    fixed suggestions int-test

--- client/int-test/suggestions.spec.js
+++ client/int-test/suggestions.spec.js
@@ -2,14 +2,15 @@ import {expect} from 'chai';
 import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 
-import {setApiKey, getAddresses, stripSuggestions} from '../src/actions/suggestions.js';
+import {getAddresses} from '../src/actions/suggestions.js';
+import {setValue} from '../src/actions/settings.js';
 import reducer from '../src/reducer.js';
 
 describe('Suggestions service', function() {
   it('basic', function() {
     const store = createStore(reducer, applyMiddleware(thunk));
-    store.dispatch(setApiKey(process.env.DADATA_KEY));
-    store.dispatch(getAddresses('Ленина'))
+    store.dispatch(setValue(['suggestions', 'address', 'apiKey'], process.env.DADATA_KEY));
+    return store.dispatch(getAddresses('Ленина'))
     .then(() => {
       const addresses = store.getState().getIn(['suggestions', 'addresses']);
       expect(addresses).to.be.array;
