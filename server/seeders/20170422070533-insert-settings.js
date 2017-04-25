60816e1aad2ae7e4441990653d0fc720f61ae804
kazatca <kazatca@gmail.com>
2017-04-25 15:46:37 +1000

    added settings seed for suggestions

--- server/seeders/20170422070533-insert-settings.js
+++ server/seeders/20170422070533-insert-settings.js
@@ -1,13 +1,42 @@
 'use strict';
 
-module.exports = {
-  up: function (queryInterface, Sequelize) {
-    return queryInterface.bulkInsert('Settings', [{
-      key: 'phone.mask',
-      value: '+7(999)999-99-99',
+var flatten = require('flat');
+require('dotenv').config({
+  path: '.env'
+});
+
+var data = {
+  phone: {
+    mask: '+7(999)999-99-99'
+  },
+  suggestions: {
+    address: {
+      apiKey: process.env.DADATA_KEY,
+      count: 10,
+      locations: [
+        {city: 'Комсомольск-на-Амуре'}, 
+        {kladr_id: '2701'}
+      ],
+      unwantedWords: ['Хабаровский край', 'Комсомольский р-н']
+    }
+  }
+};
+
+function convert(data){
+  var result = flatten(data, {safe: true});
+  return Object.keys(result).map(function(key){
+    return {
+      key: key,
+      value: JSON.stringify(result[key]),
       createdAt: new Date(),
       updatedAt: new Date()
-    }], {});
+    };
+  });
+}
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    return queryInterface.bulkInsert('Settings', convert(data), {});
   },
 
   down: function (queryInterface, Sequelize) {
